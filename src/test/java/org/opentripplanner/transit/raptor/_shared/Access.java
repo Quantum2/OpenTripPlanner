497735229467a517f491a3446e386747b7d834a6
Rafael Lopes Francisco <rafaelfrancisco@easyclick.pt>
2020-03-13 18:24:38 +0000

    Lots of changs

--- src/test/java/org/opentripplanner/transit/raptor/_shared/Access.java
+++
@@ -1,8 +0,0 @@
-package org.opentripplanner.transit.raptor._shared;
-
-class Access extends AbstractStopArrival {
-    Access(int stop, int departureTime, int arrivalTime) {
-        super(0, stop, departureTime, arrivalTime, 1000, null);
-    }
-    @Override public boolean arrivedByAccessLeg() { return true; }
-}
